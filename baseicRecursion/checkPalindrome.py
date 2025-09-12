bdb8e5c53d533b52ec6f045e7ce5120d2b3861fe
Smriti Jain <smritijain6700@gmail.com>
2025-09-13 01:54:04 +0530

    CheckPalindrome

---
+++ baseicRecursion/checkPalindrome.py
@@ -0,0 +1,32 @@
+class Palindrome:
+    def __init__(self, s):
+        self.s = s
+    
+    def checkPalindrome(self, start, end):
+        if(start >= end):
+            return True
+        
+        if(self.s[start] != self.s[end]):
+            return False
+        else:
+            return self.checkPalindrome(start + 1, end - 1)
+        
+def checkPalindrome(s, start, end):
+    if(start >= end):
+        return True
+    
+    if(s[start] != s[end]):
+        return False
+    else:
+        return checkPalindrome(s, start+1, end-1)
+    
+# oops way
+s = input()
+print("OOPS")
+obj = Palindrome(s)
+print(obj.checkPalindrome(0, len(s)-1))
+
+# simple function way
+s1 = input()
+print("Simple")
+print(checkPalindrome(s1, 0, len(s1)-1))
