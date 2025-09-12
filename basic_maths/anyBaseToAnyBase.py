fe58d44c173ff8c90e42a560d828cfadacc52138
Smriti Jain <smritijain6700@gmail.com>
2025-09-12 08:40:55 +0530

    AnyBaseToAnyBase

--- basic_maths/anyBaseToAnyBase.py
+++ basic_maths/anyBaseToAnyBase.py
@@ -1,3 +1,6 @@
+# k = no of digits in s
+# time complexity - O(k)
+# space complexity - O(1)
 def toDecimal(s, fromBase):
     digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     decimal = 0
@@ -9,14 +12,17 @@ def toDecimal(s, fromBase):
     
     return decimal
 
+# time complexity - O(log{tobase}decimal)
+# space complexity - O(1)
 def fromDecimal(decimal, toBase):
     if(decimal == 0):
         return "0"
     
+    digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     val = ""
     while(decimal > 0):
         x = decimal%toBase
-        val = str(x) + val
+        val = digits[x] + val
         decimal = decimal//toBase
     return val
 
@@ -26,3 +32,4 @@ def anyBaseToAnyBase(s, fromBase, toBase):
     return val
 
 print(anyBaseToAnyBase("A3", 16, 2))
+print(fromDecimal(255, 16))  
