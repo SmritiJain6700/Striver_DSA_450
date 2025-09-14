e74c5bdaf90068fbdcc71ce2b4229cbca5dc4be3
Smriti Jain <smritijain6700@gmail.com>
2025-09-14 23:04:50 +0530

    ReverseNum

---
+++ basic_maths/reverseNumber.py
@@ -0,0 +1,11 @@
+def reverseNumber(n):
+    if(n == 0):
+        return 0
+    revNum = 0
+    while(n != 0):
+        revNum = revNum*10 + n%10
+        n = n//10
+    return revNum
+
+n = int(input("Enter a number:"))
+print(f"Reverse of a number-{n} is {reverseNumber(n)}")
